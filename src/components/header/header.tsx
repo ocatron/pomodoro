4fccf9427b14a27578ba367a2b687a732ba657b1
Thamara Liyanage <thamara.prabuddha@gmail.com>
2024-01-26 16:51:27 +0530

    feat(header): add mode toggle

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,16 @@
 import logo from "@/assets/logo.svg";
+import { ModeToggle } from "../mode-toggle";
 
 export function Header() {
   return (
     <header>
-      <div className="flex h-14 w-full items-center border-b border-border bg-background px-4">
+      <div className="flex h-14 w-full items-center justify-between border-b border-border bg-background px-4">
         <div className="flex items-center">
           <img className="h-8 w-8" src={logo} />
-          <span className="pl-2 text-xl font-semibold">Pomodoro</span>
+          <span className="pl-1.5 text-lg font-semibold">Pomodoro</span>
+        </div>
+        <div>
+          <ModeToggle />
         </div>
       </div>
     </header>
