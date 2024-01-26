6706ecb0139de02b3462e6f334e7b0ba0240bebb
Thamara Liyanage <thamara.prabuddha@gmail.com>
2024-01-26 14:22:19 +0530

    feat(header): change background color and add border

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,8 +2,8 @@ import logo from "@/assets/logo.svg";
 
 export function Header() {
   return (
-    <header className="bg-white">
-      <div className="mx-auto flex h-14 w-full max-w-3xl items-center px-4">
+    <header>
+      <div className="flex h-14 w-full items-center border-b border-border bg-background px-4">
         <div className="flex items-center">
           <img className="h-8 w-8" src={logo} />
           <span className="pl-2 text-xl font-semibold">Pomodoro</span>
