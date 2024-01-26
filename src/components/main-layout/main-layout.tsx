77735f6437d5f2d507c955380d280542a29645cb
Thamara Liyanage <thamara.prabuddha@gmail.com>
2024-01-26 14:22:59 +0530

    feat(main-layout): change background color

--- src/components/main-layout/main-layout.tsx
+++ src/components/main-layout/main-layout.tsx
@@ -6,7 +6,7 @@ interface MainLayoutProps {
 }
 export function MainLayout({ children }: MainLayoutProps) {
   return (
-    <div className="flex min-h-screen flex-col bg-gray-100">
+    <div className="flex min-h-screen flex-col bg-background">
       <Header />
       <main className="mx-auto w-full max-w-3xl flex-grow px-4">
         {children}
