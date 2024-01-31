5b3b671c36dcaa79e8348c8851e0068f9e81c2fc
Thamara Liyanage <thamara.prabuddha@gmail.com>
2024-01-31 07:52:17 +0530

    feat: add tabs to the app

--- src/components/timer/timer.tsx
+++ src/components/timer/timer.tsx
@@ -1,10 +1,12 @@
 import { useTimer } from "@/hooks/use-timer";
 import { cn } from "@/lib/ui-utils";
-import React from "react";
+import React, { useState } from "react";
 import { Countdown } from "./countdown";
 import { addMinutes } from "date-fns";
+import { Tabs, TabsList, TabsTrigger } from "../tabs";
 
 type TimerProps = React.HTMLAttributes<HTMLDivElement>;
+type TimerMode = "pomodoro" | "short-break" | "long-break";
 
 export const Timer = React.forwardRef<HTMLDivElement, TimerProps>(
   // eslint-disable-next-line react/prop-types
@@ -13,13 +15,28 @@ export const Timer = React.forwardRef<HTMLDivElement, TimerProps>(
     const { totalSeconds } = useTimer({
       expiryTime: addMinutes(new Date(), totalMinutes),
     });
+
+    const [timerMode, setTimerMode] = useState<TimerMode>("pomodoro");
     return (
       <div
-        className={cn("rounded-md border border-border  p-4", className)}
+        className={cn(
+          "flex flex-col items-center rounded-md border border-border p-4",
+          className,
+        )}
         ref={ref}
         {...props}
       >
-        <Countdown remainingSeconds={totalSeconds} />
+        <Tabs
+          value={timerMode}
+          onValueChange={(value) => setTimerMode(value as TimerMode)}
+        >
+          <TabsList>
+            <TabsTrigger value="pomodoro">Pomodoro</TabsTrigger>
+            <TabsTrigger value="short-break">Short Break</TabsTrigger>
+            <TabsTrigger value="long-break">Long Break</TabsTrigger>
+          </TabsList>
+        </Tabs>
+        <Countdown remainingSeconds={totalSeconds} className="mt-8" />
       </div>
     );
   },
