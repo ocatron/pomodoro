796a7a26fe6352575be7d165f6de5ce6ead87dae
Thamara Liyanage <thamara.prabuddha@gmail.com>
2024-02-10 12:02:58 +0530

    feat(main-layout): fixed header bar

--- src/components/main-layout/main-layout.tsx
+++ src/components/main-layout/main-layout.tsx
@@ -1,16 +1,17 @@
 import { ReactNode } from "react";
 import { Header } from "../header";
+import { ScrollArea } from "../scroll-area";
 
 interface MainLayoutProps {
   children: ReactNode;
 }
 export function MainLayout({ children }: MainLayoutProps) {
   return (
-    <div className="flex min-h-screen flex-col bg-background">
+    <div className="flex h-screen flex-col bg-background">
       <Header />
-      <main className="mx-auto w-full max-w-3xl flex-grow px-4">
-        {children}
-      </main>
+      <ScrollArea>
+        <main className="mx-auto w-full max-w-3xl px-4 py-4">{children}</main>
+      </ScrollArea>
     </div>
   );
 }
--- src/components/mode-toggle/mode-toggle.tsx
+++ src/components/mode-toggle/mode-toggle.tsx
@@ -16,7 +16,7 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" shape="square" size="sm">
+        <Button variant="ghost" shape="square" size="sm" className="relative">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-transform dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-transform dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
